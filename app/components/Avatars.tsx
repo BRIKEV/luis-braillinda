674f32a2299524d10fe7d4104e66ebb69a471f21
kevinccbsg <kevinccbsg@gmail.com>
2025-05-10 21:25:43 +0200

    feat: add background to the story

--- app/components/Avatars.tsx
+++ app/components/Avatars.tsx
@@ -10,7 +10,7 @@ const Avatars: React.FC<AvatarsProps> = ({ author }) => {
   const activeStyles = 'opacity-100 scale-100 transition-transform duration-300';
   const narratorHidenStyles = author.toLowerCase() === 'narrador' ? 'opacity-0' : '';
   return (
-    <div className={`flex justify-between mb-2 ${narratorHidenStyles}`}>
+    <div className={`flex items-center justify-between mb-2 ${narratorHidenStyles} bg-[url('/app/images/background.png')] bg-cover bg-no-repeat bg-center h-96 w-full rounded-lg mb-4`}>
       <img
         className={`w-1/2 max-w-40 opacity-70 scale-75 ${author.toLowerCase() === 'luis' ? activeStyles : 'opacity-70'}`}
         src={luisImage}
