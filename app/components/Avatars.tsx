1abcdaf5cdcdf9a99adaa22b2e04699af016295b
kevinccbsg <kevinccbsg@gmail.com>
2025-05-11 01:09:16 +0200

    feat: add ai assets

--- app/components/Avatars.tsx
+++ app/components/Avatars.tsx
@@ -10,14 +10,14 @@ const Avatars: React.FC<AvatarsProps> = ({ author }) => {
   const activeStyles = 'opacity-100 scale-100 transition-transform duration-300';
   const narratorHidenStyles = author.toLowerCase() === 'narrador' ? 'opacity-0' : '';
   return (
-    <div className={`flex items-center justify-between mb-2 ${narratorHidenStyles} bg-[url('/app/images/background.png')] bg-cover bg-no-repeat bg-center h-96 w-full rounded-lg mb-4`}>
+    <div className={`flex items-center justify-between mb-2 ${narratorHidenStyles} bg-[url('/app/images/background.png')] bg-cover bg-no-repeat bg-center h-96 w-full rounded-lg md:justify-evenly`}>
       <img
-        className={`w-1/2 max-w-40 opacity-70 scale-75 ${author.toLowerCase() === 'luis' ? activeStyles : 'opacity-70'}`}
+        className={`max-w-50 w-50 opacity-70 scale-75 ${author.toLowerCase() === 'luis' ? activeStyles : 'opacity-70'}`}
         src={luisImage}
         alt="Maestro luis"
       />
       <img
-        className={`w-1/2 max-w-40 opacity-70 scale-75 ${author.toLowerCase() === 'braillinda' ? activeStyles : 'opacity-70'}`}
+        className={`w-1/2 max-w-40 flip-image opacity-70 scale-75 ${author.toLowerCase() === 'braillinda' ? activeStyles : 'opacity-70'}`}
         src={braillindaImage}
         alt="Hada braillinda"
       />
